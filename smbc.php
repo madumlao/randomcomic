bddc3ffb5e39b0db85a3aad009318597790e9146
madumlao <madumlao@gmail.com>
2017-11-22 22:24:53 +0800

    Update smbc script to prepend smbc domain to img src

--- smbc.php
+++ smbc.php
@@ -19,7 +19,7 @@ $obj['link'] = pq('#permalinktext')->attr('value');
 $obj['title'] = pq('title')->html();
 preg_match('!Cereal - (.*)$!', $obj['title'], $matches);
 $obj['serial'] = $matches[1];
-$obj['src'] = pq('#cc-comic')->attr('src');
+$obj['src'] = '//smbc-comics.com' . pq('#cc-comic')->attr('src');
 $obj['alt'] = pq('#cc-comic')->attr('title');
 echo json_encode($obj, JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT);
 exit();
