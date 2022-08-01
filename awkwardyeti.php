331bcf57c6e93fe62485ff3289c550761ae06d1d
madumlao <madumlao@gmail.com>
2022-08-01 14:34:53 +0800

    Update awkwardyeti

--- awkwardyeti.php
+++ awkwardyeti.php
@@ -21,7 +21,6 @@ if (!isset($_REQUEST['slug'])) {
 	$url = "http://theawkwardyeti.com/$slug";
 }
 
-
 $ch = curl_init($url);
 curl_setopt($ch, CURLOPT_ENCODING, "");
 curl_setopt($ch, CURLOPT_FOLLOWLOCATION, TRUE);
@@ -33,18 +32,24 @@ libxml_use_internal_errors(true);
 phpQuery::newDocument($response);
 
 header('Content-Type: application/json');
-foreach (pq('#comic > a > img[alt][title]') AS $comic) {
+foreach (pq('article.type-comic') AS $comic) {
 	$obj = [];
+
+	$img = pq($comic)->find('img.attachment-post-thumbnail')[0];
+	$title = pq($comic)->find('.entry-header .c-blog-title')[0];
+	$url = preg_replace('!/$!', '', $title->attr('href'));
+	$slug = preg_replace('!.*/!', '', $url);
+	$title_text = trim(pq($title)->find('h3')->text());
 
 	$obj['source'] = 'awkwardyeti';
 	$obj['source_title'] = 'The Awkward Yeti';
 	$obj['link'] = $url;
 	$obj['slug'] = $slug;
-	$obj['src'] = pq($comic)->attr('src');
+	$obj['src'] = pq($img)->attr('src');
 	preg_match('!uploads/(\d{4}/\d{2}(/\d{2})?)!', $obj['src'], $matches);
 	$obj['serial'] = $matches[1];
-	$obj['title'] = pq($comic)->attr('alt');
-	$obj['alt'] = pq($comic)->attr('title');
+	$obj['title'] = $title_text;
+	$obj['lt'] = pq($img)->attr('title');
 
 	echo json_encode($obj, JSON_UNESCAPED_SLASHES|JSON_PRETTY_PRINT);
 	exit();
